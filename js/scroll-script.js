e7e4400e0a6188808350d96697a6291c3d7381c9
Juan Carlos Sánchez G <jcarlos0305@hotmail.com>
2018-01-14 10:43:05 -0600

    Layout fix

--- js/scroll-script.js
+++ js/scroll-script.js
@@ -1,15 +1,39 @@
 $(document).ready(function () {
-    $(".navbar-li__links").click(function () {
+    $(".navbar__brand").click(function () {
         $('html, body').animate({
-            scrollTop: $(".section2__div").offset().top
+            scrollTop: $(".section1__img").offset().top
         }, 1000);
     });
 });
 
 $(document).ready(function () {
-    $(".navbar__brand").click(function () {
+    $("#link1").click(function () {
         $('html, body').animate({
-            scrollTop: $(".section1__img").offset().top
+            scrollTop: $(".section2__img").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function () {
+    $("#link2").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".section3__img").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function () {
+    $("#link3").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".section4__img").offset().top
+        }, 1000);
+    });
+});
+
+$(document).ready(function () {
+    $("#link4").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".section5__img").offset().top
         }, 1000);
     });
 });
